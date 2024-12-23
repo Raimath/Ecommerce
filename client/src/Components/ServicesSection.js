3c0c9c6e1636a5a08960b70b3f17969c0f3bd761
Raimath420 <raimath420@gmail.com>
2024-12-23 13:33:38 +0530

    updated

--- client/src/Components/ServicesSection.js
+++ client/src/Components/ServicesSection.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { easeIn, motion, spring } from "motion/react"
+import { motion } from "motion/react"
 import {NavLink} from 'react-router-dom'
 
 export const ServicesSection = () => {
